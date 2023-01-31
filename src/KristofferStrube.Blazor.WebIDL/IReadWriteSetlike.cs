d6e8f8f0168c6f361478ccd35b06a364fb067841
KristofferStrube <kristoffer.strube@gmail.com>
2023-01-31 23:41:25 +0100

    Better IReadWriteSetlike interface.

--- src/KristofferStrube.Blazor.WebIDL/IReadWriteSetlike.cs
+++ src/KristofferStrube.Blazor.WebIDL/IReadWriteSetlike.cs
@@ -2,4 +2,18 @@
 
 public interface IReadWriteSetlike<T> : IReadonlySetlike<T> where T : IJSWrapper
 {
+    public async Task<T> AddAsync(T element)
+    {
+        return await Task.FromResult<T>(default!);
+    }
+
+    public async Task<bool> ClearAsync()
+    {
+        return await Task.FromResult<bool>(default!);
+    }
+
+    public async Task<bool> DeleteAsync(T element)
+    {
+        return await Task.FromResult<bool>(default!);
+    }
 }
