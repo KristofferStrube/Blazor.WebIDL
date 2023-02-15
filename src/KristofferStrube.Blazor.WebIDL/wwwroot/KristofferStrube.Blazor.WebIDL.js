eb07cbbe3a2be7910e8c883aff7fdfc7aa7e5d65
KristofferStrube <kristoffer.strube@gmail.com>
2023-02-15 17:07:56 +0100

    Added reference to JS type method.

--- src/KristofferStrube.Blazor.WebIDL/wwwroot/KristofferStrube.Blazor.WebIDL.js
+++ src/KristofferStrube.Blazor.WebIDL/wwwroot/KristofferStrube.Blazor.WebIDL.js
@@ -12,9 +12,11 @@ export function forEachWithTwoArguments(jSReference, callbackObjRef) {
     jSReference.forEach((value, key) => callbackObjRef.invokeMethodAsync('InvokeCallback', DotNet.createJSObjectReference(value, key)))
 }
 
+// https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
 export function valuePropertiesType(obj) {
     return ({}).toString.call(obj.value).match(/\s([a-z|A-Z]+)/)[1].toLowerCase();
 }
+
 export function valuePropertiesValue(obj) {
     return obj.value;
 }
