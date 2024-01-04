65546dc4eaadbcec482c3fd3a5511a32c0bf7c32
KristofferStrube <kristoffer.strube@gmail.com>
2024-01-04 19:21:36 +0100

    Made IArrayBufferView an IJSWrapper.

--- src/KristofferStrube.Blazor.WebIDL/Buffers/IArrayBufferView.cs
+++ src/KristofferStrube.Blazor.WebIDL/Buffers/IArrayBufferView.cs
@@ -4,7 +4,7 @@
 /// A common interface for objects that provide a view on to an <see cref="ArrayBuffer"/> or <see cref="SharedArrayBuffer"/>.
 /// </summary>
 /// <remarks><see href="https://tc39.es/ecma262/multipage/structured-data.html#sec-arraybuffer-objects">See the API definition here</see>.</remarks>
-public interface IArrayBufferView
+public interface IArrayBufferView : IJSWrapper
 {
     /// <summary>
     /// Gets the internal array buffer. This can either be an <see cref="ArrayBuffer"/> or a <see cref="SharedArrayBuffer"/>.
