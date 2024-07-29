7231f4b631776ddf4940db1ffd839cd1c6b3a185
Robert McLaws <1657085+robertmclaws@users.noreply.github.com>
2024-07-29 18:19:53 -0400

    - Enhancements to WebIDLException to efficiently parse the StackTrace. - Unit tests to verify the WebIDLException behavior - Framework-specific builds for .NET 8 and .NET 7 (to prevent manually having to re-target the deprecated System.Text.Json 7.* in .NET 8 apps) - Inclusion of DirectoryBuildProps to simplify project files

---
+++ KristofferStrube.Blazor.Tests.WebIDL/Exceptions/WebIDLExceptionTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using KristofferStrube.Blazor.WebIDL.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KristofferStrube.Blazor.Tests.WebIDL.Exceptions;
+
+[TestClass]
+public class WebIDLExceptionTests
+{
+
+    /// <summary>
+    /// Tests that the <see cref="WebIDLException"/> can be constructed and that <see cref="WebIDLException.GetStackFrames" />
+    /// correctly parses the stack.
+    /// </summary>
+    [TestMethod]
+    public void WebIDLException_CanGetStackFrames_DeepStack()
+    {
+        var stacktrace = @"causeErrors@http://localhost:5010/myfunctions.js:7:9 
+        window.onmessage@http://localhost:5010/:21:17
+        EventHandlerNonNull*@http://localhost:5010/:18:9";
+
+        WebIDLException exception = new("message", stacktrace);
+        exception.Should().NotBeNull();
+        exception.Message.Should().Be("message");
+        exception.StackTrace.Should().Be(stacktrace);
+        exception.GetStackFrames().Should().NotBeNullOrEmpty().And.HaveCount(3);
+    }
+
+    /// <summary>
+    /// Tests that the <see cref="WebIDLException"/> can be constructed and that <see cref="WebIDLException.GetStackFrames" />
+    /// returns an empty list when there is no stack.
+    /// </summary>
+    [TestMethod]
+    public void WebIDLException_CanGetStackFrames_NoStack()
+    {
+        WebIDLException exception = new("message");
+        exception.Should().NotBeNull();
+        exception.Message.Should().Be("message");
+        exception.StackTrace.Should().BeNull();
+        exception.GetStackFrames().Should().NotBeNull().And.BeEmpty();
+    }
+
+}
--- src/KristofferStrube.Blazor.WebIDL/Exceptions/WebIDLException.cs
+++ src/KristofferStrube.Blazor.WebIDL/Exceptions/WebIDLException.cs
@@ -1,25 +1,64 @@
-﻿namespace KristofferStrube.Blazor.WebIDL.Exceptions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace KristofferStrube.Blazor.WebIDL.Exceptions;
 
 /// <summary>
 /// A common exception class for all exceptions from the <c>Blazor.WebIDL</c> library.
 /// </summary>
-public class WebIDLException : Exception
+public partial class WebIDLException : Exception
 {
-    private readonly string? jSStackTrace;
+
+    ///RWM: Source - https://github.com/MindscapeHQ/raygun4blazor/pull/25#discussion_r1694840772
+    [GeneratedRegex(@"(?<functionName>.+)@(?<fileName>.+):(?<lineNumber>\d+):(?<columnNumber>\d+)", RegexOptions.IgnoreCase)]
+    private static partial Regex StackFrameRegex();
+
+    private readonly string? jsStackTrace;
 
     /// <summary>
-    /// Returns the stack trace as a string. If no stack trace is available, null is returned. The stack is prepended with the JS stack trace if there is any.
+    /// Returns the stack trace as a string.
     /// </summary>
-    public override string? StackTrace => jSStackTrace is not null ? jSStackTrace + '\n' + base.StackTrace : base.StackTrace;
+    /// <remarks>
+    /// If no stack trace is available, null is returned. The stack is prepended with the JS stack trace if there is any.
+    /// </remarks>
+    public override string? StackTrace => 
+        // @robertmclaws: Doing it this way prevents unnecessary whitespace from being added to the trace, breaking tests.
+        string.IsNullOrWhiteSpace(jsStackTrace) && string.IsNullOrWhiteSpace(base.StackTrace) ?
+        null :
+        $"{jsStackTrace}{(!string.IsNullOrWhiteSpace(base.StackTrace) ? "\n" : null)}{base.StackTrace}";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns>
+    /// A <see cref="List{StackFrame}" /> containing the <see cref="StackTrace"/> parsed as a series of <see cref="StackFrame">StackFrames</see>.
+    /// </returns>
+    public List<StackFrame?> GetStackFrames()
+    {
+        if (StackTrace is null) return [];
+
+        return StackTrace
+            .Split('\n')
+            .Where(frame => !string.IsNullOrWhiteSpace(frame))
+            .Select(frame =>
+            {
+                Match match = StackFrameRegex().Match(frame);
+                return !match.Success ?
+                    null :
+                    new StackFrame(match.Groups["fileName"].Value, int.Parse(match.Groups["lineNumber"].Value), int.Parse(match.Groups["columnNumber"].Value));
+            })
+            .Where(c => c is not null)
+            .ToList();
+    }
 
     /// <summary>
     /// Constructs a wrapper Exception for the given error.
     /// </summary>
     /// <param name="message">User agent-defined value that provides human readable details of the error.</param>
-    /// <param name="jSStackTrace">The stack trace from JavaScript if there is any.</param>
+    /// <param name="jsStackTrace">The stack trace from JavaScript if there is any.</param>
     /// <param name="innerException">Inner exception which is the cause of this exception.</param>
-    public WebIDLException(string message, string? jSStackTrace, Exception innerException) : base(message, innerException)
+    public WebIDLException(string message, string? jsStackTrace = default, Exception? innerException = null) : base(message, innerException)
     {
-        this.jSStackTrace = jSStackTrace;
+        this.jsStackTrace = jsStackTrace;
     }
 }
