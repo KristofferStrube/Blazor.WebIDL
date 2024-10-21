09b4f3dd234222e2cf38c91de6a932cbc0867cf5
KristofferStrube <kristoffer.strube@gmail.com>
2024-10-21 22:58:35 +0200

    Added float type arrays.

---
+++ src/KristofferStrube.Blazor.WebIDL/TypedArrays/Float16Array.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+
+namespace KristofferStrube.Blazor.WebIDL;
+
+/// <summary>
+/// A view on to a buffer type instance that exposes it as an array of IEEE 754 floating point numbers of 2 bytes.
+/// </summary>
+/// <remarks><see href="https://webidl.spec.whatwg.org/#idl-Float16Array">See the API definition here</see>.</remarks>
+[IJSWrapperConverter]
+public class Float16Array : TypedArray<float, Float16Array>, IJSCreatable<Float16Array>
+{
+    /// <inheritdoc/>
+    public static Task<Float16Array> CreateAsync(IJSRuntime jSRuntime, IJSObjectReference jSReference)
+    {
+        return Task.FromResult(new Float16Array(jSRuntime, jSReference, new()));
+    }
+
+    /// <inheritdoc/>
+    public static Task<Float16Array> CreateAsync(IJSRuntime jSRuntime, IJSObjectReference jSReference, CreationOptions options)
+    {
+        return Task.FromResult(new Float16Array(jSRuntime, jSReference, options));
+    }
+
+    /// <inheritdoc cref="CreateAsync(IJSRuntime, IJSObjectReference, CreationOptions)"/>
+    protected Float16Array(IJSRuntime jSRuntime, IJSObjectReference jSReference, CreationOptions options) : base(jSRuntime, jSReference, options) { }
+}
---
+++ src/KristofferStrube.Blazor.WebIDL/TypedArrays/Float64Array.InProcess.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.JSInterop;
+
+namespace KristofferStrube.Blazor.WebIDL;
+
+/// <summary>
+/// A view on to a buffer type instance that exposes it as an array of IEEE 754 floating point numbers of 8 bytes.
+/// </summary>
+/// <remarks><see href="https://webidl.spec.whatwg.org/#idl-Float64Array">See the API definition here</see>.</remarks>
+[IJSWrapperConverter]
+public class Float64ArrayInProcess : Float64Array, ITypedArrayInProcess<double, Float64ArrayInProcess, Float64Array>
+{
+    /// <summary>
+    /// A lazily loaded task that evaluates to a helper module instance from the Blazor.WebIDL library.
+    /// </summary>
+    public IJSInProcessObjectReference InProcessHelper { get; }
+
+    /// <inheritdoc/>
+    public new IJSInProcessObjectReference JSReference { get; }
+
+    /// <inheritdoc/>
+    public static async Task<Float64ArrayInProcess> CreateAsync(IJSRuntime jSRuntime, IJSInProcessObjectReference jSReference)
+    {
+        return await CreateAsync(jSRuntime, jSReference, new());
+    }
+
+    /// <inheritdoc/>
+    public static async Task<Float64ArrayInProcess> CreateAsync(IJSRuntime jSRuntime, IJSInProcessObjectReference jSReference, CreationOptions options)
+    {
+        IJSInProcessObjectReference inProcessHelper = await jSRuntime.GetInProcessHelperAsync();
+        return new(jSRuntime, inProcessHelper, jSReference, options);
+    }
+
+    /// <inheritdoc/>
+    public static new async Task<Float64ArrayInProcess> CreateAsync(IJSRuntime jSRuntime)
+    {
+        return await ITypedArrayInProcess<double, Float64ArrayInProcess, Float64Array>.CreateInternalAsync(jSRuntime);
+    }
+
+    /// <inheritdoc/>
+    public static new async Task<Float64ArrayInProcess> CreateAsync<TFromElement, TFromTypedArray>(IJSRuntime jSRuntime, TypedArray<TFromElement, TFromTypedArray> typedArray) where TFromTypedArray : IJSCreatable<TFromTypedArray>
+    {
+        return await ITypedArrayInProcess<double, Float64ArrayInProcess, Float64Array>.CreateInternalAsync(jSRuntime, typedArray);
+    }
+
+    /// <inheritdoc/>
+    public static new async Task<Float64ArrayInProcess> CreateAsync(IJSRuntime jSRuntime, IArrayBuffer buffer, long? byteOffset, long? length)
+    {
+        return await ITypedArrayInProcess<double, Float64ArrayInProcess, Float64Array>.CreateInternalAsync(jSRuntime, buffer, byteOffset, length);
+    }
+
+    /// <inheritdoc/>
+    public static new async Task<Float64ArrayInProcess> CreateAsync(IJSRuntime jSRuntime, long length)
+    {
+        return await ITypedArrayInProcess<double, Float64ArrayInProcess, Float64Array>.CreateInternalAsync(jSRuntime, length);
+    }
+
+    /// <inheritdoc cref="IJSInProcessCreatable{TInProcess, T}.CreateAsync(IJSRuntime, IJSInProcessObjectReference, CreationOptions)"/>
+    protected Float64ArrayInProcess(IJSRuntime jSRuntime, IJSInProcessObjectReference inProcessHelper, IJSInProcessObjectReference jSReference, CreationOptions options) : base(jSRuntime, jSReference, options)
+    {
+        this.InProcessHelper = inProcessHelper;
+        JSReference = jSReference;
+    }
+
+    /// <inheritdoc/>
+    public new async Task<IArrayBufferInProcess> GetBufferAsync()
+    {
+        return await ITypedArrayInProcess<double, Float64ArrayInProcess, Float64Array>.GetBufferAsync(this);
+    }
+
+    /// <inheritdoc/>
+    public double At(long index)
+    {
+        return ITypedArrayInProcess<double, Float64ArrayInProcess, Float64Array>.At(this, index);
+    }
+
+    /// <inheritdoc/>
+    public long Length => ITypedArrayInProcess<double, Float64ArrayInProcess, Float64Array>.GetLength(this);
+}
