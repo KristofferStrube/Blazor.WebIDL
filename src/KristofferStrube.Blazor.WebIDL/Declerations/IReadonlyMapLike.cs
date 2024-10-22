65326148e77cce8764393fa9fd892123955a1a86
KristofferStrube <kristoffer.strube@gmail.com>
2024-10-22 22:04:40 +0200

    Updated XML Docs for MapLike.

--- src/KristofferStrube.Blazor.WebIDL/Declerations/IReadonlyMapLike.cs
+++ src/KristofferStrube.Blazor.WebIDL/Declerations/IReadonlyMapLike.cs
@@ -3,11 +3,14 @@
 namespace KristofferStrube.Blazor.WebIDL;
 
 /// <summary>
-/// Objects implementing an interface that is declared to be maplike represent an ordered map of key–value pairs, initially empty, known as that object’s map entries.
+/// Objects implementing the <see cref="IReadonlyMapLike{TMap, TKey, TValue}"/> interface are maplike and have an ordered map of key–value pairs, initially empty, known as that object’s map entries.
 /// </summary>
 /// <remarks><see href="https://webidl.spec.whatwg.org/#idl-maplike">See the API definition here</see>.</remarks>
 public interface IReadonlyMapLike<TMap, TKey, TValue> : IJSWrapper where TMap : IReadonlyMapLike<TMap, TKey, TValue> { }
 
+/// <summary>
+/// Extensions used to access members of objects that implement <see cref="IReadonlyMapLike{TMap, TKey, TValue}"/>.
+/// </summary>
 public static class IReadonlyMapLikeExtensions
 {
     public static async Task<ulong> GetSizeAsync<TMap, TKey, TValue>(this IReadonlyMapLike<TMap, TKey, TValue> map) where TMap : IReadonlyMapLike<TMap, TKey, TValue>
