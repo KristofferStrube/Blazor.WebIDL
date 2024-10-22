a9aca522a1308b83b923745ea16e74a0408ac1cf
KristofferStrube <kristoffer.strube@gmail.com>
2024-10-22 20:48:51 +0200

    Added tests for Float64Array.

---
+++ tests/IntegrationTests/Float64ArrayTest.cs
@@ -0,0 +1,299 @@
+﻿namespace IntegrationTests;
+
+public class Float64ArrayTest : JSInteropBlazorTest
+{
+    [Test]
+    public async Task CreateAsync_WithNoArguments_Succeeds()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime);
+            return array;
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeOfType<Float64Array>();
+    }
+
+    [Test]
+    public async Task CreateAsync_WithLength_CreatesArrayWithLength()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 4);
+            long length = await array.GetLengthAsync();
+            return length;
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeOfType<long>()
+            .Which.Should().Be(4);
+    }
+
+    [Test]
+    public async Task CreateAsync_WithArrayBuffer_CreatesArrayFromBuffer()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float32Array originalArray = await Float32Array.CreateAsync(EvaluationContext.JSRuntime, 4);
+            await originalArray.FillAsync(1, 1, 2);
+            await using IArrayBuffer arrayBuffer = await originalArray.GetBufferAsync();
+
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, arrayBuffer);
+            double firstIndex = await array.AtAsync(0);
+            double secondIndex = await array.AtAsync(1);
+            return (firstIndex, secondIndex);
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeOfType<(double, double)>()
+            .Which.Should().Be((0.0078125, 0));
+    }
+
+    [Test]
+    public async Task CreateAsync_WithArrayBufferAndByteOffset_CreatesArrayFromBuffer()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array originalArray = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 12);
+            await originalArray.FillAsync(1, 0, 8);
+            await using IArrayBuffer arrayBuffer = await originalArray.GetBufferAsync();
+
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, arrayBuffer, 8);
+            double sum = 0;
+            for(int i = 0; i < 2; i++)
+            {
+                sum += await array.AtAsync(i);
+            }
+            return sum;
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeOfType<double>()
+            .Which.Should().Be(2);
+    }
+
+    [Test]
+    public async Task CreateAsync_WithArrayBufferByteOffsetAndLength_CreatesArrayFromBuffer()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array originalArray = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 24);
+            await originalArray.FillAsync(1);
+            await using IArrayBuffer arrayBuffer = await originalArray.GetBufferAsync();
+
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, arrayBuffer, 8, 2);
+            double result = await array.AtAsync(0);
+
+            return result;
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeOfType<double>()
+            .Which.Should().Be(1);
+    }
+
+    [Test]
+    public async Task CreateAsync_WithTypedArray_CreatesArrayFromTypedArray()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float32Array originalArray = await Float32Array.CreateAsync(EvaluationContext.JSRuntime, 2);
+            await originalArray.FillAsync(1, 0, 1);
+            await originalArray.FillAsync(2, 1, 2);
+
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, originalArray);
+            double firstIndex = await array.AtAsync(0);
+            double secondIndex = await array.AtAsync(1);
+            return (firstIndex, secondIndex);
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeOfType<(double, double)>()
+            .Which.Should().Be((1, 2));
+    }
+
+    [Test]
+    public async Task GetBufferAsync_GetsBuffer()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 10);
+            await using IArrayBuffer buffer = await array.GetBufferAsync();
+
+            return buffer;
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeAssignableTo<IArrayBuffer>();
+    }
+
+    [Test]
+    public async Task AtAsync_WithPositiveNumber_GetsElementFromStartOfArray()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 10);
+            await array.FillAsync(10);
+            await array.FillAsync(20, 5);
+
+            double secondElement = await array.AtAsync(1);
+
+            return secondElement;
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeAssignableTo<double>()
+            .Which.Should().Be(10);
+    }
+
+    [Test]
+    public async Task AtAsync_WithNegativeNumber_GetsElementFromEndOfArray()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 10);
+            await array.FillAsync(10);
+            await array.FillAsync(20, 5);
+
+            double secondElement = await array.AtAsync(-1);
+
+            return secondElement;
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeAssignableTo<double>()
+            .Which.Should().Be(20);
+    }
+
+    [Test]
+    public async Task FillAsync_WithNoArguments_FillsEntireArray()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 10);
+
+            await array.FillAsync(10);
+
+            double firstElement = await array.AtAsync(0);
+            double lastElement = await array.AtAsync(-1);
+
+            return (firstElement, lastElement);
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeAssignableTo<(double, double)>()
+            .Which.Should().Be((10, 10));
+    }
+
+    [Test]
+    public async Task FillAsync_WithStart_FillsArrayFromStartArgument()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 10);
+
+            await array.FillAsync(10, 5);
+
+            double firstElement = await array.AtAsync(0);
+            double lastElement = await array.AtAsync(-1);
+
+            return (firstElement, lastElement);
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeAssignableTo<(double, double)>()
+            .Which.Should().Be((0, 10));
+    }
+
+    [Test]
+    public async Task FillAsync_WithStartAndEnd_FillsArrayFromStartArgumentToEndArgument()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 10);
+
+            await array.FillAsync(10, 1, 3);
+
+            double firstElement = await array.AtAsync(0);
+            double secondElement = await array.AtAsync(1);
+            double thirdElement = await array.AtAsync(2);
+            double fourthElement = await array.AtAsync(3);
+
+            return (firstElement, secondElement, thirdElement, fourthElement);
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeAssignableTo<(double, double, double, double)>()
+            .Which.Should().Be((0, 10, 10, 0));
+    }
+
+    [Test]
+    public async Task GetLengthAsync_GetsLength()
+    {
+        // Arrange
+        AfterRenderAsync = async () =>
+        {
+            await using Float64Array array = await Float64Array.CreateAsync(EvaluationContext.JSRuntime, 10);
+
+            long length = await array.GetLengthAsync();
+
+            return length;
+        };
+
+        // Act
+        await OnAfterRerenderAsync();
+
+        // Assert
+        _ = EvaluationContext.Result.Should().BeAssignableTo<long>()
+            .Which.Should().Be(10);
+    }
+}
